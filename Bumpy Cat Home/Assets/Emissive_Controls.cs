50d112bb0c8785842d1ca042b7e4ed52e2644fa4
Tanner Cobb <tannercobb359@gmail.com>
2024-02-28 22:33:45 -0500

    Lighting Changes and Map Upload v3

--- Bumpy Cat Home/Assets/Emissive_Controls.cs
+++ Bumpy Cat Home/Assets/Emissive_Controls.cs
@@ -13,11 +13,19 @@ public class Emissive_Controls : UdonSharpBehaviour
 {
     public Material material;
     public Renderer[] referenceRenderer;
+    public ReflectionProbe[] affectedProbes;
+
+    public Light[] affectedLights;
 
     public Slider r;
     public Slider g;
     public Slider b;
     public Slider intesity;
+    
+    public void Awake(){
+        Refresh();
+    }
+
     public void Emission_Toggle(){
        if(material.IsKeywordEnabled("_EMISSION")){
             material.DisableKeyword("_EMISSION");
@@ -25,16 +33,23 @@ public class Emissive_Controls : UdonSharpBehaviour
         else{
             material.EnableKeyword("_EMISSION");
         }
-        foreach(Renderer r in referenceRenderer){
-            RendererExtensions.UpdateGIMaterials(r);
-        }
-        
+        Refresh();
     }
 
     public void Update_Color(){
         material.SetColor("_EmissionColor", new Color(r.value,g.value,b.value, 1) * (intesity.value * 2));
+        foreach(Light l in affectedLights){
+            l.color = new Color(r.value,g.value,b.value, 1) * intesity.value;
+        }
+        Refresh();
+    }
+
+    public void Refresh(){
         foreach(Renderer r in referenceRenderer){
             RendererExtensions.UpdateGIMaterials(r);
+        }
+        foreach(ReflectionProbe p in affectedProbes){
+            p.RenderProbe();
         }
     }
 }
