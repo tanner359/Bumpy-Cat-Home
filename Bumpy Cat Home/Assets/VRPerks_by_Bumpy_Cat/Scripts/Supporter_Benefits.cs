34f6311c60c98c4b77e8bebb3825af0484670a5e
Tanner Cobb <tannercobb359@gmail.com>
2024-06-25 22:03:00 -0400

    Supporter Benefits Update [WIP] + Project Migration to 2022.3.22f
    - Added Local Supporter Benefits Behavior
    - Added Supporter Lists Behavior
    - Project Upgraded to Unity 2022.3.22f1

--- Bumpy Cat Home/Assets/VRPerks_by_Bumpy_Cat/Scripts/Supporter_Benefits.cs
+++ Bumpy Cat Home/Assets/VRPerks_by_Bumpy_Cat/Scripts/Supporter_Benefits.cs
@@ -26,7 +26,8 @@ namespace BumpyCat{
 public class Supporter_Benefits : UdonSharpBehaviour
 {
     //SUPPORT BENEFITS WORK BY: CROSS REFERENCING YOUR VRC+DIS ACCOUNT -> CHECKS FOR TYPE OF SUPPORTER ROLE -> PERKS ACTIVATE.
-
+    [Header("OBJECT REFERENCES")]
+    public Supporter_List[] SUPPORT_LIST; //SUPPORTER LISTS, ATTACHED TO INDIVIDUAL TMP TEXT COMPONENTS
     public VRCUrl DISCORD_MEMBERS_URL; //https://discord.com/api/v10/guilds/1235352747923210331/members/search?query=bumpy_cat
     [Header("VRC INFO")]
     public string VRC_NAME;
@@ -34,6 +35,7 @@ public class Supporter_Benefits : UdonSharpBehaviour
     public string DISCORD_NAME;
     public string DISCORD_USER_NAME;
     public string[] DISCORD_ROLES;
+    private DataToken DISCORD_MEMBERS = new DataList();
 
     public bool SERVER_BOOSTER;
 
@@ -45,26 +47,38 @@ public class Supporter_Benefits : UdonSharpBehaviour
     public string[] DISCORD_MEMBERS_NAMES = new string[0];
 
     // Start is called before the first frame update
-    public void Start()
-    {
+    public void Start(){
         VRCStringDownloader.LoadUrl(DISCORD_MEMBERS_URL, (IUdonEventReceiver)this);
         Debug.Log($"[{gameObject.name}] Fetching Discord Members...");
 
         VRC_NAME = Networking.LocalPlayer.displayName;
     }
 
-    public void Update(){
-        
+    public void Init(){
+        for(int i = 0; i < SUPPORT_LIST.Length; i++){
+            SUPPORT_LIST[i].Initialize(this);
+        }
     }
 
+    public void Update(){
+    }
+
+    /*DISCORD JSON STRUCTURE EXAMPLE: 3 DISCORD MEMBERS
+    -> STRING LOAD -> JSON DATA
+    -> JSON DATA -> DATALIST(3 MEMBERS)
+    -> MEMBER -> DATADICTIONARY(12 SETS OF DATA)
+    -> SETS W/ EMBED -> DATADICTIONARY(DATA VALUES)
+    ->
+    -> EX. TO GET NAME: 
+    -> JSONDATA(LIST)->USER[i](DICT)->USER["global_name"] 
+    */
     public override void OnStringLoadSuccess(IVRCStringDownload data){
         if(VRCJson.TryDeserializeFromJson(data.Result, out DataToken jsonData)){
             if(jsonData.TokenType == TokenType.DataDictionary){
                 jsonData.DataDictionary.TryGetValue(1, out DataToken test);
-                Debug.Log("JSON is type Dictionary: " + test);
             }
             else if(jsonData.TokenType == TokenType.DataList){ //datalist-> dictionaries -> datalist -> tokens -> data?
-                //DISCORD_MEMBERS_NAMES = new string[jsonData.DataList.Count];
+                DISCORD_MEMBERS = jsonData;
                 for (int i = 0; i < jsonData.DataList.Count;){
                     DataToken a = jsonData.DataList[i];
                     if(a.TokenType == TokenType.DataDictionary){
@@ -78,6 +92,7 @@ public class Supporter_Benefits : UdonSharpBehaviour
                     i++;
                 }
             }
+            Init();
         }
         else{
                 Debug.Log($"Failed to Deserialize json {data.Result} - {jsonData.ToString()}");
@@ -90,17 +105,63 @@ public class Supporter_Benefits : UdonSharpBehaviour
     }
 
     /// <summary> Parses the rawData and returns a DataList containing all matching key values</summary>
-    public DataList PARSE_JSON(DataToken rawData, string key){
-
-        if (rawData.TokenType == TokenType.DataDictionary){
-            
+    [RecursiveMethod]
+    public DataToken PARSE_JSON(DataToken rawData, string[] keys, int keyindex){
+        DataToken result = new DataList();
+        if (rawData.TokenType == TokenType.DataDictionary){ // is token a dictionary
+            if(rawData.DataDictionary.TryGetValue(keys[keyindex], out DataToken value)){ //try to find a value with key
+                if(value.TokenType == TokenType.DataDictionary){
+                    DataToken a = PARSE_JSON(value, keys, keyindex+1);
+                    return a;
+                }
+                return value; // return discovered value
+            }           
+            return new DataToken(TokenType.Null);
         }
         else if(rawData.TokenType == TokenType.DataList){
-
+            for(int i = 0; i < rawData.DataList.Count; i++){
+                DataToken parse = PARSE_JSON(rawData.DataList[i], keys, keyindex);
+                result.DataList.Add(parse);
+            }
+            return result; 
         }
-
-        return null;
+        Debug.Log("Supporter_Benefits.PARSE_JSON() Returned : [Code 0]");
+        return result;
     }
+    public string Get_Supporters(){
 
+        string result = Get_Supporters(SUPPORTER_ID) + Get_Supporters(SUPPORTER_PLUS_ID) + Get_Supporters(SUPPORTER_ULTIMATE_ID);
+        return result;
+    }
+    public string Get_Supporters(long role_id){
+        string result = "";
+
+        string[] names_keys = {"user", "global_name"};
+        DataList names = PARSE_JSON(DISCORD_MEMBERS, names_keys, 0).DataList;
+        
+        string[] roles_keys = {"roles"}; 
+        DataList roles = PARSE_JSON(DISCORD_MEMBERS, roles_keys , 0).DataList;
+
+        for(int i = 0; i < DISCORD_MEMBERS.DataList.Count; i++){
+            for(int j = 0; j < roles[i].DataList.Count; j++){
+                if(roles[i].DataList[j].String == role_id.ToString()){
+                    result += names[i].String + " ";
+                }
+            }
+        }
+        return result;
+    }
+    public string Get_Supporter_Names(){
+        string[] keys = {"user", "global_name"};
+        DataList result = PARSE_JSON(DISCORD_MEMBERS, keys, 0).DataList;
+        string names = "";
+        for (int i = 0; i < result.Count; i++)
+        {
+            if(result[i].TokenType != TokenType.Null){
+                names += result[i].String + " ";
+            }
+        }
+        return names;
+    }
 }
 }
---
+++ Bumpy Cat Home/Assets/VRPerks_by_Bumpy_Cat/Scripts/Supporter_List.cs
@@ -0,0 +1,17 @@
+﻿
+using BumpyCat;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using TMPro;
+using VRC.SDK3.Data;
+public class Supporter_List : UdonSharpBehaviour
+{   
+    public TMP_Text body;
+    public long roleID;
+
+    public void Initialize(Supporter_Benefits main){
+        body.text = main.Get_Supporters(roleID);
+    }
+}
